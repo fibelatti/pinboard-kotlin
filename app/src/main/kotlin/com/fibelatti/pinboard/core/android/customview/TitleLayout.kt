5d0de9cd7722c1a5f25279faeaa0745bcf76e44d
Filipe Belatti <fibelatti@gmail.com>
2019-01-15 08:11:12 +0000

    Add proto layout resources

---
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/android/customview/TitleLayout.kt
@@ -0,0 +1,39 @@
+package com.fibelatti.pinboard.core.android.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import com.fibelatti.pinboard.R
+import com.fibelatti.core.extension.visible
+import kotlinx.android.synthetic.main.layout_title.view.*
+
+class TitleLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.layout_title, this, true)
+    }
+
+    fun setTitle(@StringRes titleRes: Int) {
+        setTitle(context.getString(titleRes))
+    }
+
+    fun setTitle(title: String) {
+        textViewTitle.text = title
+    }
+
+    fun setNavigateUp(@DrawableRes iconRes: Int = R.drawable.ic_back_arrow, navigateUp: () -> Unit) {
+        buttonNavigateBack.apply {
+            setImageDrawable(ContextCompat.getDrawable(context, iconRes))
+            setOnClickListener { navigateUp() }
+            visible()
+        }
+    }
+}
