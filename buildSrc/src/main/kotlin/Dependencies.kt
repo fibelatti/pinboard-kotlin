f54fd5cf662682a0b997106ee80c1486117b5cec
Filipe Belatti <fibelatti@gmail.com>
2019-01-18 08:03:18 +0000

    Update dependencies syntax

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -9,50 +9,50 @@ object Versions {
 }
 
 object Classpaths {
-    val gradlePlugin = "com.android.tools.build:gradle:3.2.1"
-    val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
-    val jacocoPlugin = "org.jacoco:org.jacoco.core:0.8.1"
-    val dexCountPlugin = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:0.8.2"
+    const val gradlePlugin = "com.android.tools.build:gradle:3.2.1"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
+    const val jacocoPlugin = "org.jacoco:org.jacoco.core:0.8.1"
+    const val dexCountPlugin = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:0.8.2"
 }
 
 object Dependencies {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}"
-    val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0"
-    val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.0.0"
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}"
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.0"
+    const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.0.0"
 
-    val supportLibrary = "androidx.appcompat:appcompat:1.0.2"
-    val materialDesign = "com.google.android.material:material:1.0.0"
-    val supportAnnotations = "androidx.annotation:annotation:1.0.0"
-    val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha1"
-    val archComponents = "androidx.lifecycle:lifecycle-extensions:${Versions.archComponentsVersion}"
-    val archComponentsCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.archComponentsVersion}"
-    val customTabs = "androidx.browser:browser:1.0.0"
+    const val supportLibrary = "androidx.appcompat:appcompat:1.0.2"
+    const val materialDesign = "com.google.android.material:material:1.0.0"
+    const val supportAnnotations = "androidx.annotation:annotation:1.0.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha1"
+    const val archComponents = "androidx.lifecycle:lifecycle-extensions:${Versions.archComponentsVersion}"
+    const val archComponentsCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.archComponentsVersion}"
+    const val customTabs = "androidx.browser:browser:1.0.0"
 
     private const val daggerVersion = "2.17"
 
-    val dagger = "com.google.dagger:dagger:$daggerVersion"
-    val daggerCompiler = "com.google.dagger:dagger-compiler:$daggerVersion"
+    const val dagger = "com.google.dagger:dagger:$daggerVersion"
+    const val daggerCompiler = "com.google.dagger:dagger-compiler:$daggerVersion"
 
-    val moshiKotlin = "com.squareup.moshi:moshi-kotlin:1.8.0"
+    const val moshiKotlin = "com.squareup.moshi:moshi-kotlin:1.8.0"
 
     private const val retrofitVersion = "2.5.0"
 
-    val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
-    val retrofitMoshiConverter = "com.squareup.retrofit2:converter-moshi:$retrofitVersion"
-    val httpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:3.10.0"
+    const val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+    const val retrofitMoshiConverter = "com.squareup.retrofit2:converter-moshi:$retrofitVersion"
+    const val httpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:3.10.0"
 }
 
 object TestDependencies {
     private const val junit5Version = "5.3.1"
 
-    val junit = "junit:junit:4.12"
-    val junit5 = "org.junit.jupiter:junit-jupiter-api:$junit5Version"
-    val junit5Engine = "org.junit.jupiter:junit-jupiter-engine:$junit5Version"
-    val junit5Params = "org.junit.jupiter:junit-jupiter-params:$junit5Version"
-    val junitVintage = "org.junit.vintage:junit-vintage-engine:$junit5Version"
-    val testRunner = "com.android.support.test:runner:1.1.0"
-    val kotlinTest = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
-    val mockitoCore = "org.mockito:mockito-core:2.23.0"
-    val mockitoAndroid = "org.mockito:mockito-android:2.18.3"
-    val archComponentsTest = "android.arch.core:core-testing:${Versions.archComponentsVersion}"
+    const val junit = "junit:junit:4.12"
+    const val junit5 = "org.junit.jupiter:junit-jupiter-api:$junit5Version"
+    const val junit5Engine = "org.junit.jupiter:junit-jupiter-engine:$junit5Version"
+    const val junit5Params = "org.junit.jupiter:junit-jupiter-params:$junit5Version"
+    const val junitVintage = "org.junit.vintage:junit-vintage-engine:$junit5Version"
+    const val testRunner = "com.android.support.test:runner:1.1.0"
+    const val kotlinTest = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
+    const val mockitoCore = "org.mockito:mockito-core:2.23.0"
+    const val mockitoAndroid = "org.mockito:mockito-android:2.18.3"
+    const val archComponentsTest = "android.arch.core:core-testing:${Versions.archComponentsVersion}"
 }
