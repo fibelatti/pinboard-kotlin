48d622ec07233a1cf2e48e7611de1d350a8a5623
Filipe Belatti <fibelatti@gmail.com>
2019-01-22 20:48:16 +0000

    Remove unused models

--- app/src/main/kotlin/com/fibelatti/pinboard/features/posts/data/model/PostDeleteDto.kt
+++
@@ -1,3 +0,0 @@
-package com.fibelatti.pinboard.features.posts.data.model
-
-data class PostDeleteDto(val url: String)
--- app/src/main/kotlin/com/fibelatti/pinboard/features/posts/data/model/PostRequestDto.kt
+++
@@ -1,6 +0,0 @@
-package com.fibelatti.pinboard.features.posts.data.model
-
-/**
- * Can be used for /posts/recent or /posts/all
- */
-data class PostRequestDto(val tag: String)
