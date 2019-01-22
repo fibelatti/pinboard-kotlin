29d169287b0174161f96818029ca813b55218ca8
Filipe Belatti <fibelatti@gmail.com>
2019-01-22 20:29:30 +0000

    Add progress bar layout

---
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/android/customview/ProgressBar.kt
@@ -0,0 +1,22 @@
+package com.fibelatti.pinboard.core.android.customview
+
+import android.content.Context
+import android.graphics.PorterDuff
+import android.util.AttributeSet
+import android.widget.ProgressBar
+import androidx.core.content.ContextCompat
+import com.fibelatti.pinboard.R
+
+class ProgressBar @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.attr.progressBarStyle
+) : ProgressBar(context, attrs, defStyleAttr) {
+
+    init {
+        indeterminateDrawable?.setColorFilter(
+            ContextCompat.getColor(context, R.color.color_accent),
+            PorterDuff.Mode.SRC_IN
+        )
+    }
+}
