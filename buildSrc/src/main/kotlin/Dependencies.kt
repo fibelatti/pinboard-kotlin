38f2fdfee3a2a70b39dc543656810b239b115cdb
Filipe Belatti <fibelatti@gmail.com>
2019-05-04 12:33:31 +0100

    Update dependencies

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,14 +4,14 @@ object Versions {
     const val compileSdkVersion = 28
     const val buildToolsVersion = "28.0.3"
 
-    internal const val kotlinVersion = "1.3.21"
-    internal const val coroutinesVersion = "1.1.1"
+    internal const val kotlinVersion = "1.3.31"
+    internal const val coroutinesVersion = "1.2.1"
     internal const val lifecycleVersion = "2.0.0"
     internal const val roomVersion = "2.0.0"
 }
 
 object Classpaths {
-    const val gradlePlugin = "com.android.tools.build:gradle:3.2.1"
+    const val gradlePlugin = "com.android.tools.build:gradle:3.4.0"
     const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
     const val jacocoPlugin = "org.jacoco:org.jacoco.core:0.8.1"
     const val dexCountPlugin = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:0.8.2"
@@ -22,13 +22,13 @@ object Dependencies {
     const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}"
     const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
 
-    const val supportLibrary = "androidx.appcompat:appcompat:1.1.0-alpha02"
-    const val materialDesign = "com.google.android.material:material:1.1.0-alpha03"
+    const val supportLibrary = "androidx.appcompat:appcompat:1.1.0-alpha04"
+    const val materialDesign = "com.google.android.material:material:1.1.0-alpha06"
     const val supportAnnotations = "androidx.annotation:annotation:1.0.0"
-    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha1"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-alpha5"
 
-    const val coreLib = "com.fibelatti.core:core:1.0.0"
-    const val coreLibArch = "com.fibelatti.core:arch-components:1.0.0"
+    const val coreLib = "com.fibelatti.core:core:1.1.0-alpha1"
+    const val coreLibArch = "com.fibelatti.core:arch-components:1.1.0-alpha1"
 
     const val archComponents = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleVersion}"
     const val archComponentsCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycleVersion}"
@@ -38,7 +38,7 @@ object Dependencies {
 
     const val customTabs = "androidx.browser:browser:1.0.0"
 
-    private const val daggerVersion = "2.17"
+    private const val daggerVersion = "2.22.1"
 
     const val dagger = "com.google.dagger:dagger:$daggerVersion"
     const val daggerCompiler = "com.google.dagger:dagger-compiler:$daggerVersion"
@@ -58,10 +58,10 @@ object Dependencies {
 }
 
 object TestDependencies {
-    private const val junit5Version = "5.3.1"
+    private const val junit5Version = "5.4.2"
 
-    const val coreLibTest = "com.fibelatti.core:core-test:1.0.0"
-    const val coreLibArchTest = "com.fibelatti.core:arch-components-test:1.0.0"
+    const val coreLibTest = "com.fibelatti.core:core-test:1.1.0-alpha1"
+    const val coreLibArchTest = "com.fibelatti.core:arch-components-test:1.1.0-alpha1"
 
     const val junit = "junit:junit:4.12"
     const val junit5 = "org.junit.jupiter:junit-jupiter-api:$junit5Version"
