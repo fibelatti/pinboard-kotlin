15c03b94936dd76fff9646ef5476fe7c852ecee3
Filipe Belatti <fibelatti@gmail.com>
2019-06-26 20:20:49 +0100

    Add helper method to flat map all inheritors of a Sealed Class

---
+++ app/src/test/kotlin/com/fibelatti/pinboard/KotlinReflect.kt
@@ -0,0 +1,23 @@
+package com.fibelatti.pinboard
+
+import kotlin.reflect.KClass
+
+/**
+ * Returns a list of all Sealed Subclasses of the receiver, including their own Sealed Subclasses
+ */
+val <T : Any> KClass<T>.allSealedSubclasses: List<KClass<out T>>
+    get() = sealedSubclasses.flatten()
+
+/**
+ * Given a list of Sealed Subclasses will recursively create a flattened list of containing them and
+ * their children, and their children, and their children...
+ */
+fun <T : Any> List<KClass<out T>>.flatten(): List<KClass<out T>> {
+    return flatMap {
+        if (it.sealedSubclasses.isNotEmpty()) {
+            it.sealedSubclasses.flatten()
+        } else {
+            listOf(it)
+        }
+    }
+}
