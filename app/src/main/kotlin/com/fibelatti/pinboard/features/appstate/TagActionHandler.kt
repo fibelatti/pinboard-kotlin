ae4c456f5a50c5c8a3cb6ccf41bda0c0cca01c6d
Filipe Belatti <fibelatti@gmail.com>
2019-06-29 13:44:59 +0100

    Update tag action to filter always from All

--- app/src/main/kotlin/com/fibelatti/pinboard/features/appstate/TagActionHandler.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/features/appstate/TagActionHandler.kt
@@ -1,10 +1,13 @@
 package com.fibelatti.pinboard.features.appstate
 
 import android.net.ConnectivityManager
+import com.fibelatti.core.provider.ResourceProvider
+import com.fibelatti.pinboard.R
 import com.fibelatti.pinboard.core.extension.isConnected
 import javax.inject.Inject
 
 class TagActionHandler @Inject constructor(
+    private val resourceProvider: ResourceProvider,
     private val connectivityManager: ConnectivityManager?
 ) : ActionHandler<TagAction>() {
 
@@ -12,7 +15,7 @@ class TagActionHandler @Inject constructor(
         return when (action) {
             is RefreshTags -> refresh(currentContent)
             is SetTags -> setTags(action, currentContent)
-            is PostsForTag -> postsForTag(action, currentContent)
+            is PostsForTag -> postsForTag(action)
         }
     }
 
@@ -34,13 +37,15 @@ class TagActionHandler @Inject constructor(
         }
     }
 
-    private fun postsForTag(action: PostsForTag, currentContent: Content): Content {
-        return runOnlyForCurrentContentOfType<TagList>(currentContent) {
-            it.previousContent.copy(
-                searchParameters = SearchParameters(tags = listOf(action.tag)),
-                shouldLoad = true,
-                isConnected = connectivityManager.isConnected()
-            )
-        }
+    private fun postsForTag(action: PostsForTag): Content {
+        return PostList(
+            category = All,
+            title = resourceProvider.getString(R.string.posts_title_all),
+            posts = emptyList(),
+            sortType = NewestFirst,
+            searchParameters = SearchParameters(tags = listOf(action.tag)),
+            shouldLoad = true,
+            isConnected = connectivityManager.isConnected()
+        )
     }
 }
--- app/src/test/kotlin/com/fibelatti/pinboard/features/appstate/TagActionHandlerTest.kt
+++ app/src/test/kotlin/com/fibelatti/pinboard/features/appstate/TagActionHandlerTest.kt
@@ -2,10 +2,12 @@ package com.fibelatti.pinboard.features.appstate
 
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import com.fibelatti.core.provider.ResourceProvider
 import com.fibelatti.core.test.extension.mock
 import com.fibelatti.core.test.extension.shouldBe
 import com.fibelatti.pinboard.MockDataProvider.createTag
 import com.fibelatti.pinboard.MockDataProvider.mockTitle
+import com.fibelatti.pinboard.R
 import com.fibelatti.pinboard.core.extension.isConnected
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -15,10 +17,14 @@ import org.mockito.Mockito.verify
 
 internal class TagActionHandlerTest {
 
+    private val mockResourceProvider = mock<ResourceProvider>()
     private val mockConnectivityManager = mock<ConnectivityManager>()
     private val mockActiveNetworkInfo = mock<NetworkInfo>()
 
-    private val tagActionHandler = TagActionHandler(mockConnectivityManager)
+    private val tagActionHandler = TagActionHandler(
+        mockResourceProvider,
+        mockConnectivityManager
+    )
 
     val mockPreviousContent = PostList(
         category = All,
@@ -105,20 +111,10 @@ internal class TagActionHandlerTest {
     inner class PostsForTagTests {
 
         @Test
-        fun `WHEN currentContent is not TagList THEN same content is returned`() {
+        fun `WHEN postsForTag is called THEN PostList is returned and search parameters contains the tag`() {
             // GIVEN
-            val content = mock<PostList>()
-
-            // WHEN
-            val result = tagActionHandler.runAction(mock<PostsForTag>(), content)
-
-            // THEN
-            result shouldBe content
-        }
-
-        @Test
-        fun `WHEN currentContent is TagList THEN updated content is returned`() {
-            // GIVEN
+            given(mockResourceProvider.getString(R.string.posts_title_all))
+                .willReturn(mockTitle)
             given(mockConnectivityManager.activeNetworkInfo)
                 .willReturn(mockActiveNetworkInfo)
             given(mockActiveNetworkInfo.isConnected)
