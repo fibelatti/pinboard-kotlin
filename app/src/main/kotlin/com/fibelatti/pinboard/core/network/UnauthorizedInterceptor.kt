7985d5385de4bbb87c0fd898d1553f3f554d8034
Filipe Belatti <fibelatti@gmail.com>
2019-07-07 22:36:13 +0100

    Fix test that is failing intermittently (don't use GlobalScope kids)

--- app/src/main/kotlin/com/fibelatti/pinboard/core/network/UnauthorizedInterceptor.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/network/UnauthorizedInterceptor.kt
@@ -1,5 +1,6 @@
 package com.fibelatti.pinboard.core.network
 
+import androidx.annotation.VisibleForTesting
 import com.fibelatti.pinboard.features.user.domain.UserRepository
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -12,11 +13,18 @@ class UnauthorizedInterceptor @Inject constructor(
     private val userRepository: UserRepository
 ) : Interceptor {
 
-    override fun intercept(chain: Interceptor.Chain): Response =
-        chain.run { proceed(request()) }
-            .also { response ->
-                if (response.code() == HttpURLConnection.HTTP_UNAUTHORIZED) {
-                    GlobalScope.launch { userRepository.forceLogout() }
-                }
-            }
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response = chain.proceed(chain.request())
+
+        if (response.code() == HttpURLConnection.HTTP_UNAUTHORIZED) {
+            forceLogout()
+        }
+
+        return response
+    }
+
+    @VisibleForTesting
+    fun forceLogout() {
+        GlobalScope.launch { userRepository.forceLogout() }
+    }
 }
--- app/src/test/kotlin/com/fibelatti/pinboard/core/network/UnauthorizedInterceptorTest.kt
+++ app/src/test/kotlin/com/fibelatti/pinboard/core/network/UnauthorizedInterceptorTest.kt
@@ -1,29 +1,33 @@
 package com.fibelatti.pinboard.core.network
 
 import com.fibelatti.core.test.extension.mock
-import com.fibelatti.core.test.extension.safeAny
-import com.fibelatti.core.test.extension.verifySuspend
 import com.fibelatti.pinboard.features.user.domain.UserRepository
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito.never
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.verify
 import java.net.HttpURLConnection
 
 internal class UnauthorizedInterceptorTest {
 
     private val mockUserRepository = mock<UserRepository>()
 
-    private val unauthorizedInterceptor = UnauthorizedInterceptor(mockUserRepository)
+    private val unauthorizedInterceptor = spy(UnauthorizedInterceptor(mockUserRepository))
 
     private val mockChain = mock<Interceptor.Chain>()
+    private val mockRequest = mock<Request>()
     private val mockResponse = mock<Response>()
 
     @BeforeEach
     fun setup() {
-        given(mockChain.proceed(safeAny()))
+        given(mockChain.request())
+            .willReturn(mockRequest)
+        given(mockChain.proceed(mockRequest))
             .willReturn(mockResponse)
     }
 
@@ -37,7 +41,7 @@ internal class UnauthorizedInterceptorTest {
         unauthorizedInterceptor.intercept(mockChain)
 
         // THEN
-        verifySuspend(mockUserRepository) { forceLogout() }
+        verify(unauthorizedInterceptor).forceLogout()
     }
 
     @Test
@@ -50,6 +54,6 @@ internal class UnauthorizedInterceptorTest {
         unauthorizedInterceptor.intercept(mockChain)
 
         // THEN
-        verifySuspend(mockUserRepository, never()) { forceLogout() }
+        verify(unauthorizedInterceptor, never()).forceLogout()
     }
 }
