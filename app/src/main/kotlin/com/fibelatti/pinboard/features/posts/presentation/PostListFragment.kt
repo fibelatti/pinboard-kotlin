e361e262c4abb150757fe8dfe3a95673a988e553
Filipe Belatti <fibelatti@gmail.com>
2019-07-10 19:23:06 +0100

    Fix adapter behaviour when the activity stack is restarted but the resources are still in memory

--- app/src/main/kotlin/com/fibelatti/pinboard/features/posts/presentation/PostListFragment.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/features/posts/presentation/PostListFragment.kt
@@ -199,7 +199,7 @@ class PostListFragment @Inject constructor(
             setTitle(content.title, content.totalCount, content.sortType)
         }
 
-        if (!content.posts.alreadyDisplayed) {
+        if (!content.posts.alreadyDisplayed || postsAdapter.itemCount == 0) {
             recyclerViewPosts.visible()
             layoutEmptyList.gone()
 
