de61c6aecbeeea1fee58701a2595d58c6f5fdd9d
Filipe Belatti <fibelatti@gmail.com>
2019-07-13 10:02:52 +0100

    Make function no inline because proguard doesn't like it inlined

--- app/src/main/kotlin/com/fibelatti/pinboard/core/functional/ResultFrom.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/functional/ResultFrom.kt
@@ -4,5 +4,5 @@ import com.fibelatti.core.functional.Result
 import com.fibelatti.core.functional.catching
 import com.fibelatti.core.functional.retryIO
 
-suspend inline fun <T> resultFrom(crossinline block: suspend () -> T): Result<T> =
+suspend fun <T> resultFrom(block: suspend () -> T): Result<T> =
     catching { retryIO { block() } }
