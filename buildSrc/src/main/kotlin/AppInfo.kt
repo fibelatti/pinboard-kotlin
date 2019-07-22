12efbae7e9e762c0c85bb1d32183016abb901cd5
Filipe Belatti <fibelatti@gmail.com>
2019-07-22 21:48:54 +0100

    Bump version to 1.1.0

--- buildSrc/src/main/kotlin/AppInfo.kt
+++ buildSrc/src/main/kotlin/AppInfo.kt
@@ -3,7 +3,7 @@ object AppInfo {
     const val applicationId = "com.fibelatti.pinboard"
 
     private const val versionMajor = 1
-    private const val versionMinor = 0
+    private const val versionMinor = 1
     private const val versionPatch = 0
     private const val versionBuild = 0
 
