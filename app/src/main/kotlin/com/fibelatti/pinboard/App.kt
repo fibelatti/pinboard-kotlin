ced1adf2b319c89456e3de1db39940a73f5b6a92
Filipe Belatti <fibelatti@gmail.com>
2019-08-13 21:20:26 +0100

    Remove unneeded injection code

--- app/src/main/kotlin/com/fibelatti/pinboard/App.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/App.kt
@@ -5,15 +5,11 @@ import com.fibelatti.pinboard.core.di.AppComponent
 import com.fibelatti.pinboard.core.di.DaggerAppComponent
 
 class App : Application() {
+
     val appComponent: AppComponent by lazy(mode = LazyThreadSafetyMode.NONE) {
         DaggerAppComponent
             .builder()
             .application(this)
             .build()
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        appComponent.inject(this)
     }
 }
--- app/src/main/kotlin/com/fibelatti/pinboard/core/di/Injector.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/di/Injector.kt
@@ -1,10 +1,8 @@
 package com.fibelatti.pinboard.core.di
 
-import com.fibelatti.pinboard.App
 import com.fibelatti.pinboard.core.android.base.BaseActivity
 
 interface Injector {
-    fun inject(application: App)
 
     fun inject(baseActivity: BaseActivity)
 }
