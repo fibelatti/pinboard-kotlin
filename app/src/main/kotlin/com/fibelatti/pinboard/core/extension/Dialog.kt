b0dc3d28594862114804094b17e265016da01cc9
Filipe Belatti <fibelatti@gmail.com>
2019-08-21 20:54:47 +0100

    Fix alert dialog style

--- app/src/main/kotlin/com/fibelatti/pinboard/core/extension/Dialog.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/extension/Dialog.kt
@@ -6,7 +6,7 @@ import com.fibelatti.pinboard.R
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 fun Context.showStyledDialog(configuration: MaterialAlertDialogBuilder.() -> Unit) {
-    MaterialAlertDialogBuilder(this)
+    MaterialAlertDialogBuilder(this, R.style.AppTheme_AlertDialog)
         .setBackground(ContextCompat.getDrawable(this, R.drawable.background_contrast_rounded))
         .apply(configuration)
         .show()
