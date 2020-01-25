c105ad42e3eb95528e663524b853419fec7e6fed
Filipe Belatti <fibelatti@gmail.com>
2020-01-25 12:07:10 +0000

    Update usage of deprecated connectivity method

--- app/src/pinboardapi/kotlin/com/fibelatti/pinboard/core/android/ConnectivityInfoProvider.kt
+++ app/src/pinboardapi/kotlin/com/fibelatti/pinboard/core/android/ConnectivityInfoProvider.kt
@@ -1,10 +1,38 @@
 package com.fibelatti.pinboard.core.android
 
 import android.net.ConnectivityManager
-import com.fibelatti.core.extension.orFalse
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import javax.inject.Inject
+import javax.inject.Singleton
 
-class ConnectivityInfoProvider @Inject constructor(private val connectivityManager: ConnectivityManager?) {
+@Singleton
+class ConnectivityInfoProvider @Inject constructor(connectivityManager: ConnectivityManager?) {
 
-    fun isConnected(): Boolean = connectivityManager?.activeNetworkInfo?.isConnected.orFalse()
+    var hasConnection: Boolean = false
+
+    init {
+        val networkRequest = NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .addTransportType(NetworkCapabilities.TRANSPORT_VPN)
+            .build()
+
+        connectivityManager?.registerNetworkCallback(
+            networkRequest,
+            object : ConnectivityManager.NetworkCallback() {
+
+                override fun onAvailable(network: Network) {
+                    hasConnection = true
+                }
+
+                override fun onLost(network: Network) {
+                    hasConnection = false
+                }
+            }
+        )
+    }
+
+    fun isConnected(): Boolean = hasConnection
 }
