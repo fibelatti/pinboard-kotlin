228790da79a8b50ec60815e0940179dd18e96c49
Filipe Belatti <fibelatti@gmail.com>
2020-02-16 17:08:04 +0000

    Bump material to stable

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -24,7 +24,7 @@ object Dependencies {
     const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
 
     const val supportLibrary = "androidx.appcompat:appcompat:1.1.0"
-    const val materialDesign = "com.google.android.material:material:1.1.0-rc02"
+    const val materialDesign = "com.google.android.material:material:1.1.0"
     const val fragments = "androidx.fragment:fragment:1.2.0"
     const val supportAnnotations = "androidx.annotation:annotation:1.0.0"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta4"
