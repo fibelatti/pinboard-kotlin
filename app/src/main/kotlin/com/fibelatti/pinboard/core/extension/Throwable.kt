c44062d0e0ebd459510b6ba51e6f164a207e1aae
Filipe Belatti <fibelatti@gmail.com>
2022-02-26 21:57:01 +0000

    Add more exception types to detect when pinboard is down

--- app/src/main/kotlin/com/fibelatti/pinboard/core/extension/Throwable.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/extension/Throwable.kt
@@ -3,7 +3,11 @@ package com.fibelatti.pinboard.core.extension
 import java.net.UnknownHostException
 import kotlinx.coroutines.TimeoutCancellationException
 import retrofit2.HttpException
+import java.net.ConnectException
+import javax.net.ssl.SSLHandshakeException
 
 fun Throwable.isServerException(): Boolean = this is TimeoutCancellationException ||
     this is UnknownHostException ||
-    this is HttpException
+    this is HttpException ||
+    this is SSLHandshakeException ||
+    this is ConnectException
