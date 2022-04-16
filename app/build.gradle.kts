4aa8632e343e0c2d92d84386f4f712e511860e08
Filipe Belatti <fibelatti@gmail.com>
2022-04-16 19:29:36 +0100

    Bump patch version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ object AppInfo {
 
     private const val versionMajor = 1
     private const val versionMinor = 17
-    private const val versionPatch = 0
+    private const val versionPatch = 1
     private const val versionBuild = 0
 
     val versionCode: Int = (versionMajor * 1000000 + versionMinor * 10000 + versionPatch * 100 + versionBuild)
