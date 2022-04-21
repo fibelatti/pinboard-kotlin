046180364e11081b9a07d6b3378a006246d2a8f7
Filipe Belatti <fibelatti@gmail.com>
2022-04-21 19:46:48 +0100

    Update how common versions are handled
    Another attempt to make version catalogues work well with dependabot

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,17 +6,27 @@ include(":app")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
+            val kotlinVersion = "1.6.20"
+            val coroutinesVersion = "1.6.1"
+            val hiltVersion = "2.41"
+            val roomVersion = "2.4.2"
+            val moshiVersion = "1.13.0"
+            val okHttpVersion = "4.9.3"
+            val retrofitVersion = "2.9.0"
+            val fibelattiCoreVersion = "2.0.0-alpha4"
+            val junit5Version = "5.8.2"
+
             // Classpath
             library("android-gradle-plugin", "com.android.tools.build:gradle:7.1.3")
-            library("kotlin-gradle-plugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
-            library("hilt-gradle-plugin", "com.google.dagger:hilt-android-gradle-plugin:2.41")
+            library("kotlin-gradle-plugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+            library("hilt-gradle-plugin", "com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
 
             // Project dependencies
-            library("kotlin", "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.20")
-            library("kotlin-reflect", "org.jetbrains.kotlin:kotlin-reflect:1.6.20")
+            library("kotlin", "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+            library("kotlin-reflect", "org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
 
-            library("coroutines-core", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-            library("coroutines-android", "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+            library("coroutines-core", "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            library("coroutines-android", "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
             library("appcompat", "androidx.appcompat:appcompat:1.4.1")
             library("core-ktx", "androidx.core:core-ktx:1.7.0")
@@ -26,25 +36,25 @@ dependencyResolutionManagement {
             library("material", "com.google.android.material:material:1.5.0")
             library("constraint-layout", "androidx.constraintlayout:constraintlayout:2.1.3")
             library("swipe-refresh-layout", "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-            library("fibelatti-core", "com.fibelatti.core:core:2.0.0-alpha4")
-            library("fibelatti-arch-components", "com.fibelatti.core:arch-components:2.0.0-alpha4")
+            library("fibelatti-core", "com.fibelatti.core:core:$fibelattiCoreVersion")
+            library("fibelatti-arch-components", "com.fibelatti.core:arch-components:$fibelattiCoreVersion")
             library("lifecycle-java8", "androidx.lifecycle:lifecycle-common-java8:2.4.1")
             library("lifecycle-runtime-ktx", "androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
-            library("room-runtime", "androidx.room:room-runtime:2.4.2")
-            library("room-compiler", "androidx.room:room-compiler:2.4.2")
+            library("room-runtime", "androidx.room:room-runtime:$roomVersion")
+            library("room-compiler", "androidx.room:room-compiler:$roomVersion")
             library("work-runtime-ktx", "androidx.work:work-runtime-ktx:2.7.1")
             library("browser", "androidx.browser:browser:1.4.0")
-            library("hilt-android", "com.google.dagger:hilt-android:2.41")
-            library("hilt-compiler", "com.google.dagger:hilt-compiler:2.41")
+            library("hilt-android", "com.google.dagger:hilt-android:$hiltVersion")
+            library("hilt-compiler", "com.google.dagger:hilt-compiler:$hiltVersion")
 
-            library("moshi", "com.squareup.moshi:moshi-kotlin:1.13.0")
-            library("moshi-codegen", "com.squareup.moshi:moshi-kotlin-codegen:1.13.0")
+            library("moshi", "com.squareup.moshi:moshi-kotlin:$moshiVersion")
+            library("moshi-codegen", "com.squareup.moshi:moshi-kotlin-codegen:$moshiVersion")
 
-            library("okhttp", "com.squareup.okhttp3:okhttp:4.9.3")
-            library("logging-interceptor", "com.squareup.okhttp3:logging-interceptor:4.9.3")
+            library("okhttp", "com.squareup.okhttp3:okhttp:$okHttpVersion")
+            library("logging-interceptor", "com.squareup.okhttp3:logging-interceptor:$okHttpVersion")
 
-            library("retrofit", "com.squareup.retrofit2:retrofit:2.9.0")
-            library("converter-moshi", "com.squareup.retrofit2:converter-moshi:2.9.0")
+            library("retrofit", "com.squareup.retrofit2:retrofit:$retrofitVersion")
+            library("converter-moshi", "com.squareup.retrofit2:converter-moshi:$retrofitVersion")
 
             library("jsoup", "org.jsoup:jsoup:1.14.3")
             library("play-core", "com.google.android.play:core:1.10.3")
@@ -52,22 +62,22 @@ dependencyResolutionManagement {
             library("leakcanary", "com.squareup.leakcanary:leakcanary-android:2.8.1")
 
             // Test
-            library("fibelatti-core-test", "com.fibelatti.core:core-test:2.0.0-alpha4")
+            library("fibelatti-core-test", "com.fibelatti.core:core-test:$fibelattiCoreVersion")
 
             library("junit", "junit:junit:4.13.2")
 
-            library("junit5-api", "org.junit.jupiter:junit-jupiter-api:5.8.2")
-            library("junit5-engine", "org.junit.jupiter:junit-jupiter-engine:5.8.2")
-            library("junit5-params", "org.junit.jupiter:junit-jupiter-params:5.8.2")
-            library("junit5-vintage", "org.junit.vintage:junit-vintage-engine:5.8.2")
+            library("junit5-api", "org.junit.jupiter:junit-jupiter-api:$junit5Version")
+            library("junit5-engine", "org.junit.jupiter:junit-jupiter-engine:$junit5Version")
+            library("junit5-params", "org.junit.jupiter:junit-jupiter-params:$junit5Version")
+            library("junit5-vintage", "org.junit.vintage:junit-vintage-engine:$junit5Version")
 
             library("runner", "androidx.test:runner:1.4.0")
             library("truth", "com.google.truth:truth:1.1.3")
             library("mockk", "io.mockk:mockk:1.12.3")
-            library("kotlin-test-junit", "org.jetbrains.kotlin:kotlin-test-junit:1.6.20")
-            library("coroutines-test", "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.1")
+            library("kotlin-test-junit", "org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+            library("coroutines-test", "org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
             library("arch-core-testing", "androidx.arch.core:core-testing:2.1.0")
-            library("room-testing", "androidx.room:room-testing:2.4.2")
+            library("room-testing", "androidx.room:room-testing:$roomVersion")
         }
     }
 }
