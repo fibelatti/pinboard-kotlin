0f57cf53cfe323621d61e1d13ae7f2ed3eafbda0
Filipe Belatti <fibelatti@gmail.com>
2022-08-06 13:30:06 +0100

    Update banner animation APIs

--- app/src/main/kotlin/com/fibelatti/pinboard/core/extension/View.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/extension/View.kt
@@ -6,21 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AccelerateInterpolator
-import android.view.animation.AlphaAnimation
-import android.view.animation.Animation
 import android.view.animation.DecelerateInterpolator
 import android.widget.FrameLayout
 import androidx.core.view.updateLayoutParams
 import androidx.core.view.updateMargins
 import com.fibelatti.core.extension.getContentView
 import com.fibelatti.pinboard.R
-import com.fibelatti.pinboard.core.android.DefaultAnimationListener
 import com.fibelatti.pinboard.databinding.LayoutFeedbackBannerBinding
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 fun BottomAppBar.show() {
-    animate().translationY(0f)
+    animate()
+        .translationY(0f)
         .setDuration(resources.getInteger(R.integer.anim_time_default).toLong())
         .start()
 }
@@ -45,38 +43,34 @@ fun View.smoothScrollY(scrollBy: Int) {
 @Suppress("MagicNumber")
 fun View.showBanner(message: String) {
     val contentView = getContentView()
-    val banner = LayoutFeedbackBannerBinding.inflate(LayoutInflater.from(context), contentView, false).apply {
+    val banner = LayoutFeedbackBannerBinding.inflate(
+        LayoutInflater.from(context),
+        contentView,
+        false
+    ).apply {
         root.updateLayoutParams<FrameLayout.LayoutParams> {
             gravity = Gravity.CENTER_HORIZONTAL
             updateMargins(top = resources.getDimensionPixelSize(R.dimen.margin_xlarge))
         }
+        root.alpha = 0F
 
         textViewFeedback.text = message
     }.root
 
     contentView.addView(banner)
 
-    val animTime = resources.getInteger(R.integer.anim_time_long).toLong()
-    val disappearAnimation = AlphaAnimation(1F, 0F).apply {
-        duration = animTime
-        startOffset = animTime * 5
-        interpolator = AccelerateInterpolator()
-        setAnimationListener(object : DefaultAnimationListener() {
-            override fun onAnimationEnd(animation: Animation?) {
-                contentView.removeView(banner)
-            }
-        })
-    }
-
-    val appearAnimation = AlphaAnimation(0F, 1F).apply {
-        duration = animTime
-        interpolator = DecelerateInterpolator()
-        setAnimationListener(object : DefaultAnimationListener() {
-            override fun onAnimationEnd(animation: Animation?) {
-                banner.startAnimation(disappearAnimation)
-            }
-        })
-    }
-
-    banner.startAnimation(appearAnimation)
+    banner.animate()
+        .alphaBy(1F)
+        .setDuration(1_000)
+        .setInterpolator(DecelerateInterpolator())
+        .withEndAction {
+            banner.animate()
+                .alpha(0F)
+                .setDuration(1_000)
+                .setStartDelay(2_000)
+                .setInterpolator(AccelerateInterpolator())
+                .withEndAction { contentView.removeView(banner) }
+                .start()
+        }
+        .start()
 }
