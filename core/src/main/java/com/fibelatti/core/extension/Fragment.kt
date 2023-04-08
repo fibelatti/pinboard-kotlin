8009b788d0b620e991d018f99c09b64ca0084296
Filipe Belatti <fibelatti@gmail.com>
2023-04-08 10:40:25 +0100

    Replace deprecated API

--- core/src/main/java/com/fibelatti/core/extension/Fragment.kt
+++ core/src/main/java/com/fibelatti/core/extension/Fragment.kt
@@ -1,12 +1,10 @@
 package com.fibelatti.core.extension
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 
 /**
- * Shorthand function to invoke [FragmentActivity.onBackPressed] of the parent [FragmentActivity].
+ * Shorthand function to invoke `onBackPressed` of the parent activity.
  */
 fun Fragment.navigateBack() {
-    @Suppress("DEPRECATION")
-    activity?.onBackPressed()
+    activity?.onBackPressedDispatcher?.onBackPressed()
 }
