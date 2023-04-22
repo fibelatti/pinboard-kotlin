09957417e86b308ed437d1039984238834652c6e
Filipe Belatti <fibelatti@gmail.com>
2023-04-22 18:05:38 +0100

    Replace Note Details with compose

--- app/src/main/kotlin/com/fibelatti/pinboard/features/notes/presentation/NoteDetailsFragment.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/features/notes/presentation/NoteDetailsFragment.kt
@@ -4,21 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.fibelatti.core.extension.navigateBack
-import com.fibelatti.core.extension.viewBinding
-import com.fibelatti.pinboard.R
 import com.fibelatti.pinboard.core.android.base.BaseFragment
+import com.fibelatti.pinboard.core.android.composable.AppTheme
 import com.fibelatti.pinboard.core.extension.launchInAndFlowWith
-import com.fibelatti.pinboard.databinding.FragmentNoteDetailBinding
 import com.fibelatti.pinboard.features.MainState
 import com.fibelatti.pinboard.features.MainViewModel
 import com.fibelatti.pinboard.features.appstate.AppStateViewModel
-import com.fibelatti.pinboard.features.appstate.NoteDetailContent
-import com.fibelatti.pinboard.features.notes.domain.model.Note
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.onEach
 import java.util.UUID
@@ -27,28 +22,27 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class NoteDetailsFragment @Inject constructor() : BaseFragment() {
 
-    companion object {
-
-        @JvmStatic
-        val TAG: String = "NoteDetailsFragment"
-
-        private val ACTION_ID = UUID.randomUUID().toString()
-    }
-
     private val appStateViewModel: AppStateViewModel by activityViewModels()
     private val mainViewModel: MainViewModel by activityViewModels()
     private val noteDetailsViewModel: NoteDetailsViewModel by viewModels()
-
-    private val binding by viewBinding(FragmentNoteDetailBinding::bind)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View = FragmentNoteDetailBinding.inflate(inflater, container, false).root
+    ): View = ComposeView(inflater.context)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        (view as ComposeView).setContent {
+            AppTheme {
+                NoteDetailsScreen(
+                    appStateViewModel = appStateViewModel,
+                    noteDetailsViewModel = noteDetailsViewModel,
+                )
+            }
+        }
 
         mainViewModel.updateState { currentState ->
             currentState.copy(
@@ -64,10 +58,6 @@ class NoteDetailsFragment @Inject constructor() : BaseFragment() {
     }
 
     private fun setupViewModels() {
-        appStateViewModel.noteDetailContent
-            .onEach { content -> content.note.either({ getNoteDetails(content) }, ::showNote) }
-            .launchInAndFlowWith(viewLifecycleOwner)
-
         mainViewModel.navigationClicks(ACTION_ID)
             .onEach { navigateBack() }
             .launchInAndFlowWith(viewLifecycleOwner)
@@ -77,20 +67,11 @@ class NoteDetailsFragment @Inject constructor() : BaseFragment() {
             .launchInAndFlowWith(viewLifecycleOwner)
     }
 
-    private fun getNoteDetails(content: NoteDetailContent) {
-        binding.layoutProgressBar.root.isVisible = true
-        binding.layoutDetailsRoot.isGone = true
-        noteDetailsViewModel.getNoteDetails(content.id)
-    }
+    companion object {
 
-    private fun showNote(note: Note) {
-        binding.layoutProgressBar.root.isGone = true
-        binding.layoutDetailsRoot.isVisible = true
+        @JvmStatic
+        val TAG: String = "NoteDetailsFragment"
 
-        binding.textViewNoteTitle.text = note.title
-        binding.textViewNoteSavedDate.text = getString(R.string.notes_saved_at, note.createdAt)
-        binding.textViewNoteUpdatedDate.isVisible = note.updatedAt != note.createdAt
-        binding.textViewNoteUpdatedDate.text = getString(R.string.notes_updated_at, note.updatedAt)
-        binding.textViewText.text = note.text
+        private val ACTION_ID = UUID.randomUUID().toString()
     }
 }
---
+++ app/src/main/kotlin/com/fibelatti/pinboard/features/notes/presentation/NoteDetailsScreen.kt
@@ -0,0 +1,153 @@
+package com.fibelatti.pinboard.features.notes.presentation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.fibelatti.pinboard.R
+import com.fibelatti.pinboard.features.appstate.AppStateViewModel
+import com.fibelatti.pinboard.features.appstate.NoteDetailContent
+import com.fibelatti.ui.preview.ThemePreviews
+import com.fibelatti.ui.theme.ExtendedTheme
+
+@Composable
+fun NoteDetailsScreen(
+    appStateViewModel: AppStateViewModel = hiltViewModel(),
+    noteDetailsViewModel: NoteDetailsViewModel = hiltViewModel(),
+) {
+    val appState by appStateViewModel.content.collectAsStateWithLifecycle()
+    val noteDetailContent = appState as? NoteDetailContent ?: return
+    val isLoading = noteDetailContent.note.isLeft
+    val note = noteDetailContent.note.rightOrNull()
+
+    LaunchedEffect(isLoading) {
+        if (isLoading) {
+            noteDetailsViewModel.getNoteDetails(noteDetailContent.id)
+        }
+    }
+
+    NoteDetailsScreen(
+        isLoading = isLoading,
+        title = note?.title ?: "",
+        savedAt = note?.createdAt ?: "",
+        updatedAt = note?.updatedAt ?: "",
+        text = note?.text ?: "",
+    )
+}
+
+@Composable
+private fun NoteDetailsScreen(
+    isLoading: Boolean,
+    title: String,
+    savedAt: String,
+    updatedAt: String,
+    text: String,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = ExtendedTheme.colors.backgroundNoOverlay),
+    ) {
+        AnimatedVisibility(
+            visible = isLoading,
+            enter = fadeIn(),
+            exit = fadeOut(),
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(40.dp)
+                        .align(Alignment.Center),
+                    color = MaterialTheme.colorScheme.primary,
+                )
+            }
+        }
+
+        AnimatedVisibility(
+            visible = !isLoading,
+            enter = fadeIn(),
+            exit = fadeOut(),
+        ) {
+            val scrollState = rememberScrollState()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .padding(start = 16.dp, end = 16.dp, bottom = 100.dp),
+            ) {
+                Text(
+                    text = title,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                )
+
+                Text(
+                    text = stringResource(id = R.string.notes_saved_at, savedAt),
+                    modifier = Modifier.padding(top = 4.dp),
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+
+                if (updatedAt != savedAt) {
+                    Text(
+                        text = stringResource(id = R.string.notes_updated_at, updatedAt),
+                        modifier = Modifier.padding(top = 4.dp),
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+
+                Divider(
+                    modifier = Modifier.padding(vertical = 16.dp),
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+
+                Text(
+                    text = text,
+                    modifier = Modifier.padding(top = 4.dp),
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@ThemePreviews
+private fun NoteDetailScreenPreview() {
+    ExtendedTheme {
+        NoteDetailsScreen(
+            isLoading = false,
+            title = "Note title",
+            savedAt = "21/04/2023",
+            updatedAt = "22/04/2023",
+            text = LoremIpsum(1_000).values.first(),
+        )
+    }
+}
