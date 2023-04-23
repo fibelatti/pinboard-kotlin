9f3e35c73b3a7481d6739dbc01f92aacc63cba3f
Filipe Belatti <fibelatti@gmail.com>
2023-04-23 17:59:33 +0100

    Replace RenameTagDialog view with compose

--- app/src/main/kotlin/com/fibelatti/pinboard/features/tags/presentation/RenameTagDialog.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/features/tags/presentation/RenameTagDialog.kt
@@ -1,11 +1,38 @@
 package com.fibelatti.pinboard.features.tags.presentation
 
 import android.content.Context
-import android.view.inputmethod.EditorInfo
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import com.fibelatti.core.extension.hideKeyboard
-import com.fibelatti.core.extension.onActionOrKeyboardSubmit
-import com.fibelatti.pinboard.databinding.FragmentRenameTagBinding
+import com.fibelatti.pinboard.R
+import com.fibelatti.pinboard.core.extension.setThemedContent
+import com.fibelatti.pinboard.core.extension.setViewTreeOwners
 import com.fibelatti.pinboard.features.tags.domain.model.Tag
+import com.fibelatti.ui.preview.ThemePreviews
+import com.fibelatti.ui.theme.ExtendedTheme
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 object RenameTagDialog {
@@ -16,17 +43,73 @@ object RenameTagDialog {
         onRename: (Tag, String) -> Unit,
     ) {
         BottomSheetDialog(context).apply {
-            val dialogLayout = FragmentRenameTagBinding.inflate(layoutInflater)
-            setContentView(dialogLayout.root)
+            setViewTreeOwners()
 
-            val action = {
-                onRename(tag, dialogLayout.editTextNewName.text.toString())
-                dialogLayout.root.hideKeyboard()
-                dismiss()
-            }
-
-            dialogLayout.editTextNewName.onActionOrKeyboardSubmit(EditorInfo.IME_ACTION_DONE) { action() }
-            dialogLayout.buttonRename.setOnClickListener { action() }
+            setContentView(
+                ComposeView(context).apply {
+                    setThemedContent {
+                        RenameTagScreen(
+                            onRename = { newName ->
+                                onRename(tag, newName)
+                                hideKeyboard()
+                                dismiss()
+                            },
+                        )
+                    }
+                },
+            )
         }.show()
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun RenameTagScreen(
+    onRename: (String) -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+    ) {
+        var newName by rememberSaveable { mutableStateOf("") }
+        val focusRequester = remember { FocusRequester() }
+
+        OutlinedTextField(
+            value = newName,
+            onValueChange = { newValue -> newName = newValue },
+            modifier = Modifier
+                .weight(1F)
+                .focusRequester(focusRequester),
+            label = { Text(text = stringResource(id = R.string.tag_filter_hint)) },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions { onRename(newName) },
+            singleLine = true,
+            maxLines = 1,
+        )
+
+        FilledTonalButton(
+            onClick = { onRename(newName) },
+            modifier = Modifier.padding(start = 8.dp, top = 12.dp),
+        ) {
+            Text(
+                text = stringResource(id = R.string.quick_actions_rename),
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
+    }
+}
+
+@Composable
+@ThemePreviews
+private fun RenameTagScreenPreview() {
+    ExtendedTheme {
+        Box {
+            RenameTagScreen(onRename = {})
+        }
     }
 }
