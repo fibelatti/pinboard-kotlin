16e44195a61e9f0361c37823084c60dea12dfa3d
Filipe Belatti <fibelatti@gmail.com>
2023-04-25 22:59:08 +0100

    Migrate nav menu to compose

--- app/src/main/kotlin/com/fibelatti/pinboard/features/navigation/NavigationMenuFragment.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/features/navigation/NavigationMenuFragment.kt
@@ -1,37 +1,20 @@
 package com.fibelatti.pinboard.features.navigation
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.WindowCompat
-import androidx.core.view.isGone
-import androidx.core.view.isVisible
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import com.fibelatti.core.extension.shareText
-import com.fibelatti.core.extension.viewBinding
 import com.fibelatti.pinboard.R
 import com.fibelatti.pinboard.core.AppConfig
 import com.fibelatti.pinboard.core.di.AppReviewMode
 import com.fibelatti.pinboard.core.di.MainVariant
-import com.fibelatti.pinboard.databinding.FragmentMenuBinding
-import com.fibelatti.pinboard.features.appstate.All
-import com.fibelatti.pinboard.features.appstate.AppStateViewModel
-import com.fibelatti.pinboard.features.appstate.Private
-import com.fibelatti.pinboard.features.appstate.Public
-import com.fibelatti.pinboard.features.appstate.Recent
-import com.fibelatti.pinboard.features.appstate.Unread
-import com.fibelatti.pinboard.features.appstate.Untagged
-import com.fibelatti.pinboard.features.appstate.ViewNotes
-import com.fibelatti.pinboard.features.appstate.ViewPopular
-import com.fibelatti.pinboard.features.appstate.ViewPreferences
-import com.fibelatti.pinboard.features.appstate.ViewTags
-import com.fibelatti.pinboard.features.user.presentation.AuthViewModel
+import com.fibelatti.pinboard.core.extension.setThemedContent
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -43,17 +26,6 @@ class NavigationMenuFragment @Inject constructor(
     @MainVariant private val mainVariant: Boolean,
     @AppReviewMode private val appReviewMode: Boolean,
 ) : BottomSheetDialogFragment() {
-
-    companion object {
-
-        @JvmStatic
-        val TAG: String = "NavigationMenuFragment"
-    }
-
-    private val appStateViewModel: AppStateViewModel by activityViewModels()
-    private val authViewModel: AuthViewModel by viewModels()
-
-    private val binding by viewBinding(FragmentMenuBinding::bind)
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -78,97 +50,37 @@ class NavigationMenuFragment @Inject constructor(
                 }
         }
 
-        return FragmentMenuBinding.inflate(inflater, container, false).root
+        return ComposeView(inflater.context)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        try {
-            @Suppress("DEPRECATION")
-            val pInfo = view.context.packageManager.getPackageInfo(view.context.packageName, 0)
-            binding.menuItemVersion.text = getString(R.string.about_version, pInfo.versionName)
-        } catch (ignored: PackageManager.NameNotFoundException) {
-            binding.menuItemVersion.isGone = true
-        }
-
-        binding.menuItemPublic.isVisible = mainVariant
-        binding.menuItemPrivate.isVisible = mainVariant
-        binding.menuItemNotes.isVisible = mainVariant && !appReviewMode
-        binding.menuItemLogout.isVisible = mainVariant
-
-        setupCategoryListeners()
-        setupUserListeners()
-        setupFooterListeners()
-    }
-
-    private fun setupCategoryListeners() {
-        binding.menuItemAll.setOnClickListener {
-            appStateViewModel.runAction(All)
-            dismiss()
-        }
-        binding.menuItemRecent.setOnClickListener {
-            appStateViewModel.runAction(Recent)
-            dismiss()
-        }
-        binding.menuItemPublic.setOnClickListener {
-            appStateViewModel.runAction(Public)
-            dismiss()
-        }
-        binding.menuItemPrivate.setOnClickListener {
-            appStateViewModel.runAction(Private)
-            dismiss()
-        }
-        binding.menuItemUnread.setOnClickListener {
-            appStateViewModel.runAction(Unread)
-            dismiss()
-        }
-        binding.menuItemUntagged.setOnClickListener {
-            appStateViewModel.runAction(Untagged)
-            dismiss()
-        }
-        binding.menuItemTags.setOnClickListener {
-            appStateViewModel.runAction(ViewTags)
-            dismiss()
-        }
-        binding.menuItemNotes.setOnClickListener {
-            appStateViewModel.runAction(ViewNotes)
-            dismiss()
-        }
-        binding.menuItemPopular.setOnClickListener {
-            appStateViewModel.runAction(ViewPopular)
-            dismiss()
-        }
-    }
-
-    private fun setupUserListeners() {
-        binding.menuItemPreferences.setOnClickListener {
-            appStateViewModel.runAction(ViewPreferences)
-            dismiss()
-        }
-        binding.menuItemLogout.setOnClickListener {
-            authViewModel.logout()
-            dismiss()
-        }
-    }
-
-    private fun setupFooterListeners() {
-        binding.menuItemShare.setOnClickListener {
-            requireActivity().shareText(
-                R.string.share_title,
-                getString(
-                    R.string.share_text,
-                    "${AppConfig.PLAY_STORE_BASE_URL}${AppConfig.MAIN_PACKAGE_NAME}",
-                ),
+        (view as ComposeView).setThemedContent {
+            NavigationMenuScreen(
+                mainVariant = mainVariant,
+                appReviewMode = appReviewMode,
+                onShareClicked = {
+                    requireActivity().shareText(
+                        R.string.share_title,
+                        getString(
+                            R.string.share_text,
+                            "${AppConfig.PLAY_STORE_BASE_URL}${AppConfig.MAIN_PACKAGE_NAME}",
+                        ),
+                    )
+                },
+                onRateClicked = {
+                    Intent(Intent.ACTION_VIEW).apply {
+                        data = Uri.parse("${AppConfig.PLAY_STORE_BASE_URL}${AppConfig.MAIN_PACKAGE_NAME}")
+                        setPackage("com.android.vending")
+                    }.let(::startActivity)
+                },
+                onOptionSelected = { dismiss() },
             )
-            dismiss()
         }
-        binding.menuItemRate.setOnClickListener {
-            Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("${AppConfig.PLAY_STORE_BASE_URL}${AppConfig.MAIN_PACKAGE_NAME}")
-                setPackage("com.android.vending")
-            }.let(::startActivity)
-            dismiss()
-        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        val TAG: String = "NavigationMenuFragment"
     }
 }
---
+++ app/src/main/kotlin/com/fibelatti/pinboard/features/navigation/NavigationMenuScreen.kt
@@ -0,0 +1,272 @@
+package com.fibelatti.pinboard.features.navigation
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.fibelatti.pinboard.R
+import com.fibelatti.pinboard.features.appstate.All
+import com.fibelatti.pinboard.features.appstate.AppStateViewModel
+import com.fibelatti.pinboard.features.appstate.Private
+import com.fibelatti.pinboard.features.appstate.Public
+import com.fibelatti.pinboard.features.appstate.Recent
+import com.fibelatti.pinboard.features.appstate.Unread
+import com.fibelatti.pinboard.features.appstate.Untagged
+import com.fibelatti.pinboard.features.appstate.ViewNotes
+import com.fibelatti.pinboard.features.appstate.ViewPopular
+import com.fibelatti.pinboard.features.appstate.ViewPreferences
+import com.fibelatti.pinboard.features.appstate.ViewTags
+import com.fibelatti.pinboard.features.user.presentation.AuthViewModel
+import com.fibelatti.ui.preview.ThemePreviews
+import com.fibelatti.ui.theme.ExtendedTheme
+
+@Composable
+fun NavigationMenuScreen(
+    appStateViewModel: AppStateViewModel = hiltViewModel(),
+    authViewModel: AuthViewModel = hiltViewModel(),
+    mainVariant: Boolean = true,
+    appReviewMode: Boolean = false,
+    onShareClicked: () -> Unit,
+    onRateClicked: () -> Unit,
+    onOptionSelected: () -> Unit,
+) {
+    NavigationMenuScreen(
+        mainVariant = mainVariant,
+        appReviewMode = appReviewMode,
+        onAllClicked = {
+            appStateViewModel.runAction(All)
+            onOptionSelected()
+        },
+        onRecentClicked = {
+            appStateViewModel.runAction(Recent)
+            onOptionSelected()
+        },
+        onPublicClicked = {
+            appStateViewModel.runAction(Public)
+            onOptionSelected()
+        },
+        onPrivateClicked = {
+            appStateViewModel.runAction(Private)
+            onOptionSelected()
+        },
+        onReadLaterClicked = {
+            appStateViewModel.runAction(Unread)
+            onOptionSelected()
+        },
+        onUntaggedClicked = {
+            appStateViewModel.runAction(Untagged)
+            onOptionSelected()
+        },
+        onTagsClicked = {
+            appStateViewModel.runAction(ViewTags)
+            onOptionSelected()
+        },
+        onNotesClicked = {
+            appStateViewModel.runAction(ViewNotes)
+            onOptionSelected()
+        },
+        onPopularClicked = {
+            appStateViewModel.runAction(ViewPopular)
+            onOptionSelected()
+        },
+        onPreferencesClicked = {
+            appStateViewModel.runAction(ViewPreferences)
+            onOptionSelected()
+        },
+        onLogoutClicked = {
+            authViewModel.logout()
+            onOptionSelected()
+        },
+        onShareClicked = {
+            onShareClicked()
+            onOptionSelected()
+        },
+        onRateClicked = {
+            onRateClicked()
+            onOptionSelected()
+        },
+    )
+}
+
+@Composable
+private fun NavigationMenuScreen(
+    mainVariant: Boolean = true,
+    appReviewMode: Boolean = false,
+    onAllClicked: () -> Unit,
+    onRecentClicked: () -> Unit,
+    onPublicClicked: () -> Unit,
+    onPrivateClicked: () -> Unit,
+    onReadLaterClicked: () -> Unit,
+    onUntaggedClicked: () -> Unit,
+    onTagsClicked: () -> Unit,
+    onNotesClicked: () -> Unit,
+    onPopularClicked: () -> Unit,
+    onPreferencesClicked: () -> Unit,
+    onLogoutClicked: () -> Unit,
+    onShareClicked: () -> Unit,
+    onRateClicked: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .nestedScroll(rememberNestedScrollInteropConnection())
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = 16.dp, vertical = 32.dp)
+            .navigationBarsPadding(),
+    ) {
+        MenuItem(textRes = R.string.menu_navigation_all, onClick = onAllClicked)
+
+        MenuItem(textRes = R.string.menu_navigation_recent, onClick = onRecentClicked)
+
+        if (mainVariant) {
+            MenuItem(textRes = R.string.menu_navigation_public, onClick = onPublicClicked)
+
+            MenuItem(textRes = R.string.menu_navigation_private, onClick = onPrivateClicked)
+        }
+
+        MenuItem(textRes = R.string.menu_navigation_unread, onClick = onReadLaterClicked)
+
+        MenuItem(textRes = R.string.menu_navigation_untagged, onClick = onUntaggedClicked)
+
+        MenuItem(textRes = R.string.menu_navigation_tags, onClick = onTagsClicked)
+
+        if (mainVariant && !appReviewMode) {
+            MenuItem(textRes = R.string.menu_navigation_notes, onClick = onNotesClicked)
+        }
+
+        MenuItem(textRes = R.string.menu_navigation_popular, onClick = onPopularClicked)
+
+        Divider(
+            modifier = Modifier.padding(vertical = 16.dp),
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+
+        MenuItem(
+            textRes = R.string.menu_navigation_preferences,
+            onClick = onPreferencesClicked,
+            iconRes = R.drawable.ic_preferences,
+        )
+
+        if (mainVariant) {
+            MenuItem(
+                textRes = R.string.menu_navigation_logout,
+                onClick = onLogoutClicked,
+                iconRes = R.drawable.ic_person,
+            )
+        }
+
+        Divider(
+            modifier = Modifier.padding(vertical = 16.dp),
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+
+        MenuItem(
+            textRes = R.string.about_share,
+            onClick = onShareClicked,
+            textStyle = MaterialTheme.typography.bodySmall,
+        )
+
+        MenuItem(
+            textRes = R.string.about_rate,
+            onClick = onRateClicked,
+            textStyle = MaterialTheme.typography.bodySmall,
+        )
+
+        Text(
+            text = stringResource(id = R.string.about_developer),
+            modifier = Modifier.padding(top = 8.dp),
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontFamily = FontFamily(Font(R.font.jetbrainsmono)),
+            style = MaterialTheme.typography.bodySmall,
+        )
+
+        runCatching {
+            val context = LocalContext.current
+
+            @Suppress("DEPRECATION")
+            val pInfo = context.packageManager.getPackageInfo(context.packageName, 0)
+
+            Text(
+                text = stringResource(R.string.about_version, pInfo.versionName),
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                fontFamily = FontFamily(Font(R.font.jetbrainsmono)),
+                style = MaterialTheme.typography.bodySmall,
+            )
+        }
+    }
+}
+
+@Composable
+private fun MenuItem(
+    @StringRes textRes: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = MaterialTheme.typography.bodyMedium,
+    @DrawableRes iconRes: Int? = null,
+) {
+    TextButton(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(all = 0.dp),
+    ) {
+        if (iconRes != null) {
+            Icon(
+                painter = painterResource(id = iconRes),
+                contentDescription = stringResource(id = textRes),
+                modifier = Modifier.padding(end = 16.dp),
+                tint = MaterialTheme.colorScheme.onSurface,
+            )
+        }
+
+        Text(
+            text = stringResource(id = textRes),
+            modifier = Modifier.weight(1f),
+            color = MaterialTheme.colorScheme.onSurface,
+            style = textStyle,
+        )
+    }
+}
+
+@Composable
+@ThemePreviews
+private fun NavigationMenuScreenPreview() {
+    ExtendedTheme {
+        NavigationMenuScreen(
+            onAllClicked = {},
+            onRecentClicked = {},
+            onPublicClicked = {},
+            onPrivateClicked = {},
+            onReadLaterClicked = {},
+            onUntaggedClicked = {},
+            onTagsClicked = {},
+            onNotesClicked = {},
+            onPopularClicked = {},
+            onPreferencesClicked = {},
+            onLogoutClicked = {},
+            onShareClicked = {},
+            onRateClicked = {},
+        )
+    }
+}
