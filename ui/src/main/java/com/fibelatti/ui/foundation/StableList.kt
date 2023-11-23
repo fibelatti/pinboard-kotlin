0a4d2836782e1b996ede24b8b52971b687fdbe4c
Filipe Belatti <fibelatti@gmail.com>
2023-11-23 01:06:06 +0000

    Further API changes to `StableList`

--- ui/src/main/java/com/fibelatti/ui/foundation/StableList.kt
+++ ui/src/main/java/com/fibelatti/ui/foundation/StableList.kt
@@ -1,16 +1,25 @@
 package com.fibelatti.ui.foundation
 
 import androidx.compose.runtime.Immutable
+import java.util.Collections
 
 /**
- * An [Immutable] class that wraps a [List] of [T] in order to provide stability to a composable
- * when needed. The given `value` is assumed to be immutable.
+ * An [Immutable] wrapper class that delegates to a [List] of [T] in order to provide stability to
+ * a composable when needed.
  */
 @Immutable
-class StableList<T> internal constructor(value: List<T>) : List<T> by value
+class StableList<T> private constructor(items: List<T>) : List<T> by items {
+
+    companion object {
+
+        operator fun <T> invoke(
+            value: List<T>,
+        ): StableList<T> = StableList(items = Collections.unmodifiableList(value))
+    }
+}
 
 fun <T> stableListOf(vararg items: T): StableList<T> = StableList(value = items.toList())
 
-fun <T> List<T>.toStableList(): StableList<T> = StableList(value = toList())
+fun <T> List<T>.toStableList(): StableList<T> = StableList(value = this)
 
-fun <T> Array<T>.toStableList(): StableList<T> = StableList(value = toList())
+fun <T> Array<T>.toStableList(): StableList<T> = StableList(value = this.toList())
