1837f0c0251b10286c43d4466fa1d96fc06dec26
Filipe Belatti <fibelatti@gmail.com>
2024-06-16 10:03:18 +0100

    Remove unused dependencies from core-android

--- core-android/build.gradle.kts
+++ core-android/build.gradle.kts
@@ -17,23 +17,10 @@ android {
 }
 
 dependencies {
-    implementation(projects.core)
-
     implementation(libs.kotlin)
-    implementation(libs.coroutines.core)
 
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.viewbinding)
-
-    testCompileOnly(libs.junit)
-    testRuntimeOnly(libs.junit5.engine)
-    testRuntimeOnly(libs.junit5.vintage)
-    testImplementation(libs.junit5.api)
-    testImplementation(libs.junit5.params)
-
-    testImplementation(libs.truth)
-    testImplementation(libs.mockk)
-    testImplementation(libs.coroutines.test)
 }
