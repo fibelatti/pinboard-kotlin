2b9aa1a698f9533b47ffe49175ccdfa8bd9ea691
Filipe Belatti <fibelatti@gmail.com>
2025-02-02 15:09:36 +0000

    Update error report email

--- app/src/main/kotlin/com/fibelatti/pinboard/core/android/base/BaseActivity.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/android/base/BaseActivity.kt
@@ -2,6 +2,7 @@ package com.fibelatti.pinboard.core.android.base
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.annotation.CallSuper
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
 import com.fibelatti.pinboard.BuildConfig
 import com.fibelatti.pinboard.R
+import com.fibelatti.pinboard.core.android.composable.ErrorReportEntryPoint
 import com.fibelatti.pinboard.core.di.modules.ActivityEntryPoint
 import com.fibelatti.pinboard.core.extension.applySecureFlag
 import com.fibelatti.pinboard.core.extension.isServerException
@@ -49,6 +51,12 @@ fun FragmentActivity.sendErrorReport(
     altMessage: String = "",
     postAction: () -> Unit = {},
 ) {
+    val entryPoint = EntryPointAccessors.fromApplication(
+        applicationContext,
+        ErrorReportEntryPoint::class.java,
+    )
+    val appModeProvider = entryPoint.appModeProvider()
+
     MaterialAlertDialogBuilder(this).apply {
         val message = altMessage.ifEmpty { getString(R.string.error_report_rationale) }
 
@@ -61,14 +69,19 @@ fun FragmentActivity.sendErrorReport(
             val sw = StringWriter()
             throwable.printStackTrace(PrintWriter(sw))
 
-            val emailBody = "Hi, can you please look into this report?" +
-                "\n\nMy app version is ${BuildConfig.VERSION_NAME}" +
-                "\n\n$sw"
-            val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
-                data = Uri.parse("mailto:")
-                putExtra(Intent.EXTRA_EMAIL, arrayOf("fibelatti+dev@gmail.com"))
-                putExtra(Intent.EXTRA_SUBJECT, "Pinkt - Error Report")
-                putExtra(Intent.EXTRA_TEXT, emailBody)
+            val emailBody = StringBuilder().apply {
+                appendLine("Android Version: ${Build.VERSION.RELEASE} (SDK ${Build.VERSION.SDK_INT})")
+                appendLine("Current Service: ${appModeProvider.appMode.value}")
+                appendLine("---")
+                appendLine("This error just happened to me:")
+                appendLine()
+                append(sw.toString().replace(regex = "&?auth_token=[^&]*".toRegex(), replacement = ""))
+            }
+
+            val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:")).apply {
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("appsupport@fibelatti.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "Pinkt (${BuildConfig.VERSION_NAME}) — Error Report")
+                putExtra(Intent.EXTRA_TEXT, emailBody.toString())
             }
             startActivity(Intent.createChooser(emailIntent, getString(R.string.error_send_email)))
             dialog?.dismiss()
