fb3d6a3ad3d4657207037ba6b91363d64f7e3271
Filipe Belatti <fibelatti@gmail.com>
2025-02-02 15:38:28 +0000

    Update `isServerException` to check the cause too

--- app/src/main/kotlin/com/fibelatti/pinboard/core/extension/Throwable.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/extension/Throwable.kt
@@ -4,6 +4,12 @@ import io.ktor.client.plugins.ResponseException
 import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.io.IOException
 
-fun Throwable.isServerException(): Boolean = this is ResponseException ||
-    this is IOException ||
-    this is TimeoutCancellationException
+fun Throwable.isServerException(): Boolean {
+    val serverTypes = listOf(
+        ResponseException::class,
+        IOException::class,
+        TimeoutCancellationException::class,
+    )
+
+    return this::class in serverTypes || cause?.let { it::class in serverTypes } == true
+}
