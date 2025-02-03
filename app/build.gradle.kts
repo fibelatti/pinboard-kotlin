d1a0a0b132df95a1a696bc8fe6e4f5533ca5b4c0
Filipe Belatti <fibelatti@gmail.com>
2025-02-03 15:37:18 +0000

    Bump version to 3.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ object AppInfo {
     const val APPLICATION_ID = "com.fibelatti.pinboard"
 
     private const val VERSION_MAJOR = 3
-    private const val VERSION_MINOR = 5
+    private const val VERSION_MINOR = 6
     private const val VERSION_PATCH = 0
     private const val VERSION_BUILD = 0
 
