71d1b75a4b4186e086bc4ce0aa5a5e4d90d10ddc
Filipe Belatti <fibelatti@gmail.com>
2025-03-02 13:50:57 +0000

    Add new `RetainedLifecycleScope` for ViewModels

--- app/src/main/kotlin/com/fibelatti/pinboard/core/di/modules/AndroidModule.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/di/modules/AndroidModule.kt
@@ -5,14 +5,22 @@ import android.net.ConnectivityManager
 import androidx.core.content.getSystemService
 import com.fibelatti.core.android.platform.AppResourceProvider
 import com.fibelatti.core.android.platform.ResourceProvider
+import com.fibelatti.pinboard.core.android.RetainedLifecycleScope
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.ViewModelLifecycle
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import java.text.Collator
 import java.util.Locale
 import javax.inject.Singleton
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -30,4 +38,20 @@ object AndroidModule {
     @Provides
     @Singleton
     fun resourceProvider(@ApplicationContext context: Context): ResourceProvider = AppResourceProvider(context)
+}
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object ViewModelScopeModule {
+
+    @Provides
+    @ViewModelScoped
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
+
+    @Provides
+    @ViewModelScoped
+    fun provideViewModelCoroutineScope(lifecycle: ViewModelLifecycle): CoroutineScope {
+        return RetainedLifecycleScope(context = SupervisorJob())
+            .also(lifecycle::addOnClearedListener)
+    }
 }
