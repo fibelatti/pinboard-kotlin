12edfb0a8bda0b769a47aad6ac5645cbad039b4c
Filipe Belatti <fibelatti@gmail.com>
2026-01-13 18:29:28 +0000

    Update `ScrollDirection` to use `produceState`

--- app/src/main/kotlin/com/fibelatti/pinboard/core/extension/ScrollDirection.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/core/extension/ScrollDirection.kt
@@ -5,12 +5,12 @@ import android.view.ViewTreeObserver
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
@@ -29,18 +29,16 @@ enum class ScrollDirection {
 
 @Composable
 fun LazyListState.rememberScrollDirection(autoResetTime: Long = 2_000): State<ScrollDirection> {
-    var isScrolling by remember { mutableStateOf(isScrollInProgress) }
-    var previousIndex by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
-    var previousScrollOffset by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
-
-    LaunchedEffect(Unit) {
+    val isScrolling: Boolean by produceState(isScrollInProgress) {
         snapshotFlow { isScrollInProgress }
             .mapLatest { scrolling ->
                 if (!scrolling) delay(autoResetTime)
                 scrolling
             }
-            .collectLatest { isScrolling = it }
+            .collectLatest { value = it }
     }
+    var previousIndex: Int by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset: Int by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
 
     return remember(this) {
         derivedStateOf {
