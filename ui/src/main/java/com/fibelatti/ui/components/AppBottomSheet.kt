726d50c14efc0294c0310cf4a6d9e6d3fe16866a
Filipe Belatti <fibelatti@gmail.com>
2026-06-01 16:07:57 +0100

    Fix import statements

--- ui/src/main/java/com/fibelatti/ui/components/AppBottomSheet.kt
+++ ui/src/main/java/com/fibelatti/ui/components/AppBottomSheet.kt
@@ -7,7 +7,8 @@ import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
-import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
