4e6f6e61d88868052cb3b5f1e45fcd9f48606dad
Filipe Belatti <fibelatti@gmail.com>
2026-08-01 19:10:46 +0100

    Remove unused `ResourceProvider` from `NoteDetailsReducer`

--- app/src/main/kotlin/com/fibelatti/pinboard/features/main/reducer/NoteDetailsReducer.kt
+++ app/src/main/kotlin/com/fibelatti/pinboard/features/main/reducer/NoteDetailsReducer.kt
@@ -1,14 +1,11 @@
 package com.fibelatti.pinboard.features.main.reducer
 
-import com.fibelatti.core.android.platform.ResourceProvider
 import com.fibelatti.pinboard.features.appstate.AppState
 import com.fibelatti.pinboard.features.appstate.NoteDetailContent
 import com.fibelatti.pinboard.features.main.MainState
 import javax.inject.Inject
 
-class NoteDetailsReducer @Inject constructor(
-    private val resourceProvider: ResourceProvider,
-) : MainStateReducer {
+class NoteDetailsReducer @Inject constructor() : MainStateReducer {
 
     override fun invoke(mainState: MainState, appState: AppState): MainState {
         if (appState.content !is NoteDetailContent) return mainState
